05fa6fa31ca3508479c9de6c464aec06d4088fc5
Abhi97shek <abhi.kumar736@gmail.com>
2022-09-28 23:07:10 +0530

    CORS setup completed

--- backend/app.js
+++ backend/app.js
@@ -3,56 +3,35 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Product = require("./modal/ProductSchema");
+const cors = require('cors');
 const PORT = 3000;
 require('dotenv').config();
-const api = process.env.API_URL;
+
+app.use(cors());
+app.options('*',cors);
+
+// Routes
+const productRoutes = require("./routes/products");
+const categoryRotes = require('./routes/categories');
+const orderRoutes = require("./routes/orders");
+const userRoutes = require("./routes/users");
 
 // MiddleWare
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
+const api = process.env.API_URL;
 
-// GET API to get all the Products
+// API Routes
+app.use(`${api}/products`,productRoutes);
+app.use(`${api}/categories`,categoryRotes);
+app.use(`${api}/users`,userRoutes);
+app.use(`${api}/orders`,orderRoutes);
 
-app.get(`${api}/products`,async (req,res)=>{
 
-    const productList = await Product.find();
 
-    if(!productList)
-    {
-        res.status(500).json({success:false})
-    }
-    else
-    {
-        res.status(201).send(productList);
-    }
-
-});
-
-// POST api to Create the Product
-
-app.post(`${api}/products`,(req,res)=>{
-
-    
-    const product = new Product({
-        name:req.body.name,
-        image:req.body.image,
-        countInStock:req.body.countInStock
-    });
-    
-    product.save().then((createdProduct)=>{
-        res.status(201).json(createdProduct)
-    }).catch((err)=>{
-        res.status(500).json({
-            error:err,
-            success:failed
-        });
-    });
-
-});
-
+// DB connection
 
 mongoose.connect(process.env.CONNECTION_STRING,{
     dbName:"eshop_database"
